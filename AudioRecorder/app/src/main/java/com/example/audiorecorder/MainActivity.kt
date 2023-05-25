e2d4f4e34de6aac746a223b113383da99357c7b5
lena <olena.bykova93@gmail.com>
2023-05-25 12:05:04 +0200

    Recording Permission Request

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.audiorecorder
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.app.ActivityCompat
 
+const val REQUEST_CODE = 111
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnRecord: ImageButton
+
+    private var permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRecord = findViewById<ImageButton>(R.id.btnRecord)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+        btnRecord.setOnClickListener {
+            startRecording()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording(){
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
     }
 }
