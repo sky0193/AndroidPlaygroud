a99c492969e4a62351c64e89f2845fe75db3c3b1
lena <olena.bykova93@gmail.com>
2023-05-30 14:26:47 +0200

    finisch save view

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -5,13 +5,21 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -27,6 +35,13 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
     private lateinit var btnList: ImageButton
     private lateinit var tvTimer: TextView
     private lateinit var waveformView: WaveformView
+    private lateinit var bottomSheetBG: View
+    private lateinit var bottomSheet: LinearLayout
+    private lateinit var filenameInput: TextInputEditText
+    private lateinit var btnCancel: MaterialButton
+    private lateinit var btnOk: MaterialButton
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
     private var permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
@@ -51,6 +66,16 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
         btnList = findViewById<ImageButton>(R.id.btnList)
         tvTimer = findViewById<TextView>(R.id.tvTimer)
         waveformView = findViewById<WaveformView>(R.id.waveformView)
+
+        bottomSheetBG = findViewById<View>(R.id.bottomSheetBG)
+        bottomSheet =  findViewById<LinearLayout>(R.id.bottomSheet)
+        filenameInput = findViewById<TextInputEditText>(R.id.filenameInput)
+        btnCancel = findViewById<MaterialButton>(R.id.btnCancel)
+        btnOk = findViewById<MaterialButton>(R.id.btnOk)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
         timer = Timer(this)
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
@@ -78,18 +103,62 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
         btnDone.setOnClickListener {
             stopRecording()
             Toast.makeText(this, "Record saved", Toast.LENGTH_SHORT).show()
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBG.visibility = View.VISIBLE
+            filenameInput.setText(filename)
         }
 
         btnDelete.setOnClickListener {
             stopRecording()
-            File("$dirPath$filename.mp3")
+            File("$dirPath$filename.mp3").delete()
             Toast.makeText(this, "Record deleted", Toast.LENGTH_SHORT).show()
 
+        }
+
+        btnCancel.setOnClickListener {
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
+        }
+
+        btnOk.setOnClickListener{
+            dismiss()
+            save()
+        }
+
+        bottomSheetBG.setOnClickListener{
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
         }
 
         btnDelete.isClickable = false
     }
 
+    private fun save(){
+        val newFilename = filenameInput.text.toString()
+        if(newFilename != filename){
+            var newFile = File("$dirPath$newFilename.mp3")
+            File("$dirPath$filename.mp3").renameTo(newFile)
+        }
+    }
+
+    /**
+     * Hides Keyboard
+     * Hides View of the bottom_sheet.xml
+     */
+    private fun dismiss(){
+
+        bottomSheetBG.visibility = View.GONE
+        hideKeyboard(filenameInput)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, 100)
+
+    }
+    private fun hideKeyboard(view: View){
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
